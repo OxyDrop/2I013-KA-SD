7402691a1f067857978d0e1078bec4183c69f5c0
OxyDrop <d.serero.pro@gmail.com>
2019-04-06 04:49:45 +0200

    Lancement personnalisé

--- applications/simpleworld/MyEcosystem.java
+++ applications/simpleworld/MyEcosystem.java
@@ -5,16 +5,33 @@
 package applications.simpleworld;
 
 import graphics.Landscape;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JOptionPane;
 import worlds.*;
 
 public class MyEcosystem {
-    
+	
+	static ImageIcon imIc;
+	
 	public static void main(String[] args) {
 		
+		try {
+			BufferedImage bi = ImageIO.read(new File("earthmap1k.png"));
+			 imIc = new ImageIcon(bi);
+		} catch (IOException ex) {
+			System.out.println(ex.getMessage());
+		}
+		
 		int choix = Integer.parseInt(JOptionPane.showInputDialog(null,
-					"\tMode de lancement\n\tChoississez une option :\n1>Fenetre\n2>Console\n3>Normal\n4>Graphique"),
-					JOptionPane.INFORMATION_MESSAGE);
+					"\tMode de lancement\n\tChoississez une option :\n1>Fenetre\n2>Console\n3>Normal\n4>Graphique",
+					JOptionPane.INFORMATION_MESSAGE));
 		switch(choix)
 		{
 			case 1: 	
@@ -28,76 +45,89 @@ public class MyEcosystem {
 								"random3.png", "volcano1.png", "volcano2.png"};
 	
 				int choixW =	Integer.parseInt(JOptionPane.showInputDialog(null,
-								"Choisir Monde\n\t1>Tree\n2>Sand\n3>Snow"),
-								JOptionPane.INFORMATION_MESSAGE);
+								"Choisir Monde\n\t1>Tree\n2>Sand\n3>Snow",
+								JOptionPane.INFORMATION_MESSAGE));
 				
 				int choixRF =	Integer.parseInt(JOptionPane.showInputDialog(null,
-								"Landscape en : \n\t1>Fichier\n2>Bruit de Perlin"),
-								JOptionPane.INFORMATION_MESSAGE);
+								"Landscape en : \n\t1>Fichier\n2>Bruit de Perlin",
+								JOptionPane.INFORMATION_MESSAGE));
+				
 				
 				switch(choixRF)
 				{
 					case 1: 
+						String file = (String)JOptionPane.showInputDialog(null,"Choississez un fichier", 
+						"Choix fichier", 0, imIc ,path, path[0]);					
+						switch(choixW)
+						{
+							case 1: Landscape.run(new Landscape(wTree,file,0.6,0.35)); break;
+							case 2: Landscape.run(new Landscape(wSand,file,0.6,0.2)); break;
+							case 3: Landscape.run(new Landscape(wSnow,file,0.6,0.2)); break;
+							default:
+								JOptionPane.showMessageDialog(null, "Entrée incorrecte, réessayez");
+								System.exit(1);
+						}
 						break;
-					case 2: break;
+			
+					case 2: 
+						switch(choixW)
+						{
+							case 1 : Landscape.run(new Landscape(wTree,200,200,0.6,0.35)); break;
+							case 2 : Landscape.run(new Landscape(wSand,200,200,0.6,0.2));break;
+							case 3 : Landscape.run(new Landscape(wSnow,200,200,0.6,0.2)); break;
+							default:
+								JOptionPane.showMessageDialog(null, "Entrée incorrecte, réessayez");
+								System.exit(1);
+						}
+						break;
+						
 					default:
 						JOptionPane.showMessageDialog(null, "Entrée incorrecte, réessayez");
 						System.exit(1);
 				}
-				
-				Landscape myLandscapeGen = new Landscape(wTree,200,200, 0.8, 0.2);
-				Landscape.run(myLandscapeGen);
 				break;
-			case 2: break;
-			case 3: break;
+				
+			case 2:
+				Scanner sc = new Scanner(System.in);
+				System.out.print("Initialisation\n\tfalse:Fichier\n\ttrue:Random\n>");
+				boolean choixfile = sc.nextBoolean();
+				
+				System.out.print("\n Altitude : ");
+				double altitude = sc.nextDouble();
+				
+				System.out.print("\n Hauteur de l'eau : ");
+				double waterLevel = sc.nextDouble();
+				
+				Landscape.run(new Landscape(new WorldOfTrees(),200,200,altitude,waterLevel));
+				break;
+				
+			case 3: 
+				Landscape.run(new Landscape(new WorldOfTrees(),200,200,0.6,0.35)); 
+				break;
+				
+			case 4: 
+				DialogAppli dialog = new DialogAppli();
+				Transfert info = new Transfert();
+				dialog.LanceDialog(info);
+				
+				if(info.file)
+				{ 
+					System.out.println("okfile");
+					Landscape myLandscape = new Landscape(info.choosen, "landscape_default-200.png", info.altitude, info.waterlevel);
+					Landscape.run(myLandscape);
+				}
+				
+				else if(info.random)
+				{
+					System.out.println("okrand");
+					Landscape myLandscapeGen = new Landscape(info.choosen,200,200, info.altitude, info.waterlevel);
+					Landscape.run(myLandscapeGen);
+				}
+				break;
+				
 			default :
 				JOptionPane.showMessageDialog(null, "Entrée incorrecte, réessayez");
 				System.exit(1);
 		}
-	
-		DialogAppli dialog = new DialogAppli();
-		Transfert info = new Transfert();
-		dialog.LanceDialog(info);
-		if(info.file)
-		{ 
-			System.out.println("okfile");
-			Landscape myLandscape = new Landscape(info.choosen, "landscape_default-200.png", info.altitude, info.waterlevel);
-			Landscape.run(myLandscape);
-		}
-				
-		else if(info.random)
-		{
-			System.out.println("okrand");
-			Landscape myLandscapeGen = new Landscape(info.choosen,200,200, info.altitude, info.waterlevel);
-			Landscape.run(myLandscapeGen);
-		}
-		
-		//
-		
-		
-		
-	/*
-		// parametres:
-		// 1: le "monde" (ou sont definis vos automates cellulaires et agents
-		// 2: (ca d�pend de la methode : generation aleatoire ou chargement d'image)
-		// 3: l'amplitude de l'altitude (plus la valeur est �lev�e, plus haute sont les montagnes)
-		// 4: la quantite d'eau
-		//Landscape myLandscape = new Landscape(myWorld, 128, 128, 0.1, 0.7);
-		drole_de_texture-800-600.png
-		Fractal1_200.png
-		Fractal2_200.png
-		gouffre.png
-		landscape_canyon-128.png
-		landscape_default-128.png
-		landscape_default-200.png
-		landscape_default2-128.png
-		landscape_paris-200.png
-		landscape_volcan-200.png
-		Perlin1_200.png
-		Perlin2_200.png
-		Perlin3_200.png
-		randomLS.png
-		randomLS1.png
-		randomLS2.png*/
-    }
+	}
 }
